cb86935fc4dab36cd417d0d09a1c05c6adda380a
huangbin <huangbinhustei@gmail.com>
2018-10-07 21:37:58 +0800

    增加了七牛云的存储

--- pic.py
+++ pic.py
@@ -4,6 +4,7 @@ import time
 import cv2
 import psutil
 import requests
+import base64
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 from io import BytesIO
@@ -29,7 +30,7 @@ def get_cpu_info():
 
 
 def is_face_in(frame):
-    faces = face_cascade.detectMultiScale(
+    return face_cascade.detectMultiScale(
         cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY),
         scaleFactor=1.15,
         minNeighbors=5,
@@ -37,11 +38,31 @@ def is_face_in(frame):
     )
 
 
+def is_famaly(path):
+    with open(path, 'rb') as f:
+        b64s = str(base64.b64encode(f.read()), 'utf-8')
+    res = face.search(b64s, 'BASE64', 'Famaly', {'max_user_num': 3})
+    if res['error_code'] != 0:
+        print(res)
+        return False
+    pr = ''
+    door = 0
+    for info in res['result']['user_list']:
+        pr += info['user_id'] + ':' + str(round(info['score'],1)) + '%\t' 
+        door = max(door, info['score'])
+
+    print(pr)
+    if door >= 80:
+        return True
+    else:
+        return False
+
+
 def recording(msg, doc=False, internet=False):
     print(msg)
     if doc:
-        with open(os.path.join(base, 'recording.txt'), 'w') as f:
-            f.write(msg)
+        with open(os.path.join(base, 'recording.txt'), 'a') as f:
+            f.write(msg + '\n')
         
     if internet:
         info = msg.split('\t')
@@ -50,14 +71,20 @@ def recording(msg, doc=False, internet=False):
         send_msg(title, cont)
 
 
-def taking(max=20, limit=True):
+def drawing(img, faces):
+    for x, y, w, h in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (50, 255, 255), 1)
+    cv2.putText(img, time.ctime(), (10, 20), 1, 1, (50, 255, 255), 1, cv2.LINE_AA)
+
+
+def taking():
     print('开始保护')
-    fcount = 0
     big_step = 120
     small_step = 10
+    big = int(time.time()) + big_step
+    small = int(time.time()) + small_step
+    
     max_temp = 80
-    last_save = int(time.time()) - big_step
-    safe = True
 
     with PiCamera() as camera:
         camera.resolution = size
@@ -66,46 +93,40 @@ def taking(max=20, limit=True):
         raw_capture.truncate(0)
         for frame in camera.capture_continuous(raw_capture, format='bgr', use_video_port=True):
             raw_capture.truncate(0)
-            time.sleep(1)
             temperature, percent = get_cpu_info()
+
+            msg = [time.ctime(), temperature, percent]
             now = int(time.time())
             if float(temperature) >= max_temp:
-                recording('\t'.join(['温度太高', time.ctime(), temperature, percent]), doc=True, internet=True)
+                msg.append('温度太高')
+                recording('\t'.join(msg), doc=True, internet=True)
                 exit()
-            fcount += 1
-            if limit and fcount >= max:
-                break
             img = frame.array
-            faces = face_cascade.detectMultiScale(
-                cv2.cvtColor(img, cv2.COLOR_BGR2GRAY),
-                scaleFactor=1.15,
-                minNeighbors=5,
-                minSize=(15, 15),
-            )
+            faces = is_face_in(img)
 
             if isinstance(faces, tuple):
-                if not safe:
-                    recording('\t'.join(['人离开了', time.ctime(), temperature, percent]), doc=True)
-                    last_save = int(time.time()) - big_step
-                    safe = True
+                # 如果没有发现人脸
+                time.sleep(1)
                 continue
-            safe = False
-            if now - last_save >= big_step:
-                # 距离上次发现人很久
-                recording('\t'.join(['第一次发现', time.ctime(), temperature, percent]), doc=True, internet=True)
-                for x, y, w, h in faces:
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (50, 255, 255), 1)
-                cv2.putText(img, time.ctime(), (10, 20), 1, 1, (50, 255, 255), 1, cv2.LINE_AA)
-                cv2.imwrite(os.path.join(base, 'Persons', str(now) + '.jpg'), img)
-                last_save = now
-            elif now - last_save <= small_step:
-                # 发现人 n 帧内，连续拍照
-                recording('\t'.join(['连续拍照', time.ctime(), temperature, percent]))
-                cv2.imwrite(os.path.join(base, 'Persons', str(now) + '.jpg'), img)
+
+            if small <= now <= big: 
+                msg.append(str(big_step) + '秒内不拍照')
+                recording('\t'.join(msg), doc=True)
             else:
-                # 超过了 n 帧，且不足 m 帧，不用拍照了
-                recording('\t'.join([str(big_step) + '帧内不拍照', time.ctime(), temperature, percent]))
+                if now > big:
+                    # 距离上次发现人很久，重置时间窗口
+                    small = now + small_step
+                    big = now + big_step
+                drawing(img, faces)
+                file_path = os.path.join(base, 'Persons', str(now) + '.jpg')
+                cv2.imwrite(file_path, img)
+                if is_famaly(file_path):
+                    msg.append('欢迎回家')
+                else:
+                    msg.append('发现有人')
+                    qiniu_put(file_path, time.ctime(), bucket_name='bxin', timeout=3600)
+                recording('\t'.join(msg), doc=True, internet=True)
 
 
 if __name__ == '__main__':
-    taking(limit=False)
+    taking()
