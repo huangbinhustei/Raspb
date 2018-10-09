b0b042d8d67c12c20a3b49af56b829283f277394
huangbin <huangbinhustei@gmail.com>
2018-10-09 22:45:34 +0800

    修改时间戳

--- pic.py
+++ pic.py
@@ -3,30 +3,27 @@ import sys
 import time
 import cv2
 import psutil
-import requests
 import base64
 from picamera import PiCamera
 from picamera.array import PiRGBArray
-from io import BytesIO
 
 
-base = '/home/pi/Bxin/Raspb'
-sys.path.append(base)
+basedir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(basedir)
 from bxin import *
 
-xml = os.path.join(base, 'haarcascade_frontalface_default.xml')
+xml = os.path.join(basedir, 'haarcascade_frontalface_default.xml')
 face_cascade = cv2.CascadeClassifier(xml)
 resX = 640
 resY = 480
 size = (resX, resY)
 
 
-def get_cpu_info():
+def get_cpu_temperature():
     # Return CPU temperature as a character string
     res = os.popen('vcgencmd measure_temp').readline()
     temperature = res.replace('temp=', '').replace("'C\n", "")
-    percent = psutil.cpu_percent()
-    return temperature, str(percent)
+    return temperature
 
 
 def is_face_in(frame):
@@ -38,7 +35,7 @@ def is_face_in(frame):
     )
 
 
-def is_famaly(path):
+def is_famaly(path, min_score=80):
     with open(path, 'rb') as f:
         b64s = str(base64.b64encode(f.read()), 'utf-8')
     res = face.search(b64s, 'BASE64', 'Famaly', {'max_user_num': 3})
@@ -48,33 +45,31 @@ def is_famaly(path):
     pr = ''
     door = 0
     for info in res['result']['user_list']:
-        pr += info['user_id'] + ':' + str(round(info['score'],1)) + '%\t' 
+        pr += info['user_id'] + ':' + str(round(info['score'], 1)) + '%\t'
         door = max(door, info['score'])
 
     print(pr)
-    if door >= 80:
-        return True
-    else:
-        return False
+    return door >= min_score
 
 
 def recording(msg, doc=False, internet=False):
-    print(msg)
+    print('\t'.join(msg))
     if doc:
-        with open(os.path.join(base, 'recording.txt'), 'a') as f:
-            f.write(msg + '\n')
-        
+        with open(os.path.join(basedir, 'recording.txt'), 'a') as f:
+            f.write('\t'.join(msg) + '\n')
+
     if internet:
-        info = msg.split('\t')
-        title = info[0]
-        cont = '\n\n'.join(info[1:])
+        title = '\t'.join(msg)
+        cont = '\n\n'.join(msg)
         send_msg(title, cont)
 
 
 def drawing(img, faces):
+    color = (50, 255, 255)
     for x, y, w, h in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (50, 255, 255), 1)
-    cv2.putText(img, time.strftime('%Y%m%d_%X'), (10, 20), 1, 1, (50, 255, 255), 1, cv2.LINE_AA)
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 1)
+    name = time.strftime('%Y%m%d_%X')
+    cv2.putText(img, name, (10, 20), 1, 1, color, 1, cv2.LINE_AA)
 
 
 def taking():
@@ -83,7 +78,7 @@ def taking():
     small_step = 10
     big = int(time.time()) + big_step
     small = int(time.time()) + small_step
-    
+
     max_temp = 80
 
     with PiCamera() as camera:
@@ -93,13 +88,13 @@ def taking():
         raw_capture.truncate(0)
         for frame in camera.capture_continuous(raw_capture, format='bgr', use_video_port=True):
             raw_capture.truncate(0)
-            temperature, percent = get_cpu_info()
+            temperature = get_cpu_temperature()
 
-            msg = [time.strftime('%Y%m%d_%X'), temperature, percent]
+            msg = [time.strftime('%Y%m%d_%X')]
             now = int(time.time())
             if float(temperature) >= max_temp:
                 msg.append('温度太高')
-                recording('\t'.join(msg), doc=True, internet=True)
+                recording(msg, doc=True, internet=True)
                 exit()
             img = frame.array
             faces = is_face_in(img)
@@ -109,23 +104,24 @@ def taking():
                 time.sleep(1)
                 continue
 
-            if small <= now <= big: 
+            if small <= now <= big:
                 msg.append(str(big_step) + '秒内不拍照')
-                recording('\t'.join(msg), doc=True)
+                recording(msg, doc=True)
             else:
                 if now > big:
                     # 距离上次发现人很久，重置时间窗口
                     small = now + small_step
                     big = now + big_step
                 drawing(img, faces)
-                file_path = os.path.join(base, 'Persons', str(now) + '.jpg')
+                file_path = os.path.join(basedir, 'Persons', str(now) + '.jpg')
                 cv2.imwrite(file_path, img)
                 if is_famaly(file_path):
                     msg.append('欢迎回家')
                 else:
                     msg.append('发现有人')
-                    qiniu_put(file_path, time.strftime('%Y%m%d_%X'), bucket_name='bxin', timeout=3600)
-                recording('\t'.join(msg), doc=True, internet=True)
+                    name = time.strftime('%Y%m%d_%X') + '.jpg'
+                    qiniu_put(file_path, name, bucket_name='bxin', timeout=3600)
+                recording(msg, doc=True, internet=True)
 
 
 if __name__ == '__main__':
