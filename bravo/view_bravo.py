8f221739a3dc7864868100c486115b45f62eadad
huangbinhustei <huangbinhustei@gmail.com>
2018-12-03 21:06:16 +0800

    view_bravo

--- bravo/view_bravo.py
+++ bravo/view_bravo.py
@@ -35,17 +35,31 @@ def connect():
         print('请检查 Redis 是否打开')
         return False, False
 
-# 'cpu_temp': cpu_temp,
-# 'cpu_percent': cpu_percent,
-# 'free_ram': free_ram,
-# 'free_disk': free_disk,
-# 'zero_cpu_temp': zero_cpu_temp,
-# 'zero_free_ram': zero_free_ram,
-# 'zero_free_disk': zero_free_disk })
 
 @app.route('/info')
-def view_info():
-    hour_ago = time.time() - 3600
+@app.route('/info/<range>')
+def view_info(range=False):
+    if not range:
+        start = time.time() - 1800
+        gap = 60 - 5
+        stampType = '%H:%M'
+    elif range == 'day':
+        start = time.time() - 86400
+        gap = 3600 - 60
+        stampType = '%d %H'
+    elif range == 'week':
+        start = time.time() - 86400 * 7
+        gap = 86400 / 4 - 60
+        stampType = '%d %H'
+    elif range == 'month':
+        start = time.time() - 86400 * 30
+        gap = 86400 - 60
+        stampType = '%m-%d'
+    else:
+        print('Range Error')
+        start = time.time() - 1800
+        gap = 60
+        stampType = '%H:%M'
     flag, rdb = connect()
     assert flag
 
@@ -60,21 +74,59 @@ def view_info():
     disk_b = []
     disk_z = []
 
-    intKeys = [int(key) for key in rdb.keys() if int(key) > hour_ago]
+    tmp = [], [], [], [], [], [], []
+
+    intKeys = [int(key) for key in rdb.keys() if int(key) > start]
+    gapper = start
 
     if not intKeys:
         return ('<h2>sth wrong')
 
-    for key in sorted(intKeys):
-        stamps.append(time.strftime('%H:%M', time.gmtime(key + 28800)))
-        tmpD = rdb.hgetall(str(key))
-        cpuP_b.append(tmpD['cpu_percent'])
-        cpuT_b.append(tmpD['cpu_temp'])
-        cpuT_z.append(tmpD['zero_cpu_temp'])
-        ram_b.append(tmpD['free_ram'])
-        ram_z.append(tmpD['zero_free_ram'])
-        disk_b.append(tmpD['free_disk'])
-        disk_z.append(tmpD['zero_free_disk'])
+    if gap == 60 - 5:
+        for key in sorted(intKeys):
+            stamps.append(time.strftime(stampType, time.gmtime(key + 28800)))
+            tmpD = rdb.hgetall(str(key))
+            cpuP_b.append(tmpD['cpu_percent'])
+            cpuT_b.append(tmpD['cpu_temp'])
+            cpuT_z.append(tmpD['zero_cpu_temp'])
+            ram_b.append(tmpD['free_ram'])
+            ram_z.append(tmpD['zero_free_ram'])
+            disk_b.append(tmpD['free_disk'])
+            disk_z.append(tmpD['zero_free_disk'])
+    else:
+        for key in sorted(intKeys):
+            if key - gapper >= gap:
+                gapper = key
+                if tmp[0]:
+                    stamps.append(time.strftime(stampType, time.gmtime(key + 28800)))
+                    cpuP_b.append(round(sum(tmp[0])/len(tmp[0]), 0))
+                    cpuT_b.append(round(sum(tmp[1])/len(tmp[0]), 0))
+                    cpuT_z.append(round(sum(tmp[2])/len(tmp[0]), 0))
+                    ram_b.append(round(sum(tmp[3])/len(tmp[0]), 0))
+                    ram_z.append(round(sum(tmp[4])/len(tmp[0]), 0))
+                    disk_b.append(round(sum(tmp[5])/len(tmp[0]), 0))
+                    disk_z.append(round(sum(tmp[6])/len(tmp[0]), 0))
+                    tmp = [], [], [], [], [], [], []
+            else:
+                tmpD = rdb.hgetall(str(key))
+                tmp[0].append(float(tmpD['cpu_percent']))
+                tmp[1].append(float(tmpD['cpu_temp']))
+                tmp[2].append(float(tmpD['zero_cpu_temp']))
+                tmp[3].append(float(tmpD['free_ram']))
+                tmp[4].append(float(tmpD['zero_free_ram']))
+                tmp[5].append(float(tmpD['free_disk']))
+                tmp[6].append(float(tmpD['zero_free_disk']))
+        if tmp[0]:
+            stamps.append(time.strftime(stampType, time.gmtime(key + 28800)))
+            cpuP_b.append(sum(tmp[0])/len(tmp[0]))
+            cpuT_b.append(sum(tmp[1])/len(tmp[0]))
+            cpuT_z.append(sum(tmp[2])/len(tmp[0]))
+            ram_b.append(sum(tmp[3])/len(tmp[0]))
+            ram_z.append(sum(tmp[4])/len(tmp[0]))
+            disk_b.append(sum(tmp[5])/len(tmp[0]))
+            disk_z.append(sum(tmp[6])/len(tmp[0]))
+            tmp = [], [], [], [], [], [], []
+
     
     line_cpu.add('使用率', stamps, cpuP_b, line_width=2, is_smooth=True)
     line_cpu.add('温度_3B', stamps, cpuT_b, line_width=2, is_smooth=True)
@@ -210,197 +262,197 @@ def api_zero_dvalue():
         return jsonify(errorcode=-1, msg='检查树莓派 Zero 是否启动了 flask')
 
 
-def get_info(start, end):
-    records = dict()
-    s = DBSession()
-    for info in s.query(Info).filter(and_(Info.now >= start,
-                                          Info.now <= end)).all():
-        records[info.now // 60] = info
-    s.close()
-    return records
+# def get_info(start, end):
+#     records = dict()
+#     s = DBSession()
+#     for info in s.query(Info).filter(and_(Info.now >= start,
+#                                           Info.now <= end)).all():
+#         records[info.now // 60] = info
+#     s.close()
+#     return records
 
 
-def single_line_maker(start, end):
-    # 子函数：获取数据
-    # 己函数：把数据放到 Line 中
+# def single_line_maker(start, end):
+#     # 子函数：获取数据
+#     # 己函数：把数据放到 Line 中
 
-    def lining_single():
-        records = get_info(start, end)
+#     def lining_single():
+#         records = get_info(start, end)
 
-        stamps = []
-        cpu_percents = []
-        cpu_temps = []
-        free_rams = []
-        free_disks = []
-        for i in range(start + 60, end, 60):
-            stamps.append(time.strftime('%H:%M', time.gmtime(i + 28800)))
-            if i // 60 in records:
-                cpu_percents.append(records[i // 60].cpu_percent)
-                cpu_temps.append(records[i // 60].cpu_temp)
-                free_rams.append(records[i // 60].free_ram)
-                free_disks.append(records[i // 60].free_disk)
-            else:
-                cpu_percents.append('')
-                cpu_temps.append('')
-                free_rams.append('')
-                free_disks.append('')
+#         stamps = []
+#         cpu_percents = []
+#         cpu_temps = []
+#         free_rams = []
+#         free_disks = []
+#         for i in range(start + 60, end, 60):
+#             stamps.append(time.strftime('%H:%M', time.gmtime(i + 28800)))
+#             if i // 60 in records:
+#                 cpu_percents.append(records[i // 60].cpu_percent)
+#                 cpu_temps.append(records[i // 60].cpu_temp)
+#                 free_rams.append(records[i // 60].free_ram)
+#                 free_disks.append(records[i // 60].free_disk)
+#             else:
+#                 cpu_percents.append('')
+#                 cpu_temps.append('')
+#                 free_rams.append('')
+#                 free_disks.append('')
 
-        return stamps, cpu_percents, cpu_temps, free_rams, free_disks
+#         return stamps, cpu_percents, cpu_temps, free_rams, free_disks
 
-    line_cpu = Line(width=width, height=height, title='CPU 监控')
-    line_space = Line(width=width, height=height, title='DISK 监控')
-    if time.localtime().tm_hour >= 21 or time.localtime().tm_hour <= 6:
-        line_cpu.use_theme('dark')
-        line_space.use_theme('dark')
-    stamps, cpu_percents, cpu_temps, free_rams, free_disks = lining_single()
-    line_cpu.add('使用率', stamps, cpu_percents, mark_line=["average"], line_width=2, is_smooth=True)
-    line_cpu.add('温度', stamps, cpu_temps, mark_point=["max"], line_width=2, is_smooth=True)
-    line_space.add('剩余内存', stamps, free_rams, mark_line=["average"], line_width=2, is_smooth=True)
-    line_space.add('剩余磁盘', stamps, free_disks, line_width=2, is_smooth=True)
+#     line_cpu = Line(width=width, height=height, title='CPU 监控')
+#     line_space = Line(width=width, height=height, title='DISK 监控')
+#     if time.localtime().tm_hour >= 21 or time.localtime().tm_hour <= 6:
+#         line_cpu.use_theme('dark')
+#         line_space.use_theme('dark')
+#     stamps, cpu_percents, cpu_temps, free_rams, free_disks = lining_single()
+#     line_cpu.add('使用率', stamps, cpu_percents, mark_line=["average"], line_width=2, is_smooth=True)
+#     line_cpu.add('温度', stamps, cpu_temps, mark_point=["max"], line_width=2, is_smooth=True)
+#     line_space.add('剩余内存', stamps, free_rams, mark_line=["average"], line_width=2, is_smooth=True)
+#     line_space.add('剩余磁盘', stamps, free_disks, line_width=2, is_smooth=True)
 
-    js_list = list(set(line_cpu.get_js_dependencies() + line_space.get_js_dependencies()))
+#     js_list = list(set(line_cpu.get_js_dependencies() + line_space.get_js_dependencies()))
 
-    return line_cpu, line_space, js_list
-
-
-def group_line_maker(start, end, group_by):
-    # 子函数：获取数据
-    # 己函数：把数据放到 Line 中
-    step = {
-        'minute': 2,
-        'hour': 3540,
-        'day': 86340
-    }[group_by]
-
-    stamps = []
-    max_cpu_percent = []
-    ave_cpu_percent = []
-    max_cpu_temp = []
-    ave_cpu_temp = []
-    min_ram = []
-    ave_ram = []
-    sd = []
-    records = get_info(start, end)
-
-    for i in range(start + 60, end, step + 60):
-        stamps.append(time.strftime('%d %H:%M', time.gmtime(i + 28800 - step)))
-        this_hour = [v for r, v in records.items() if v.now > i and v.now < i + step]
-        if not this_hour:
-            max_cpu_percent.append(0)
-            ave_cpu_percent.append(0)
-            max_cpu_temp.append(0)
-            ave_cpu_temp.append(0)
-            min_ram.append(0)
-            ave_ram.append(0)
-            sd.append(0)
-        else:
-            max_cpu_percent.append(max([x.cpu_percent for x in this_hour]))
-            ave_cpu_percent.append(sum([x.cpu_percent for x in this_hour])/len(this_hour))
-            max_cpu_temp.append(max([x.cpu_temp for x in this_hour]))
-            ave_cpu_temp.append(sum([x.cpu_temp for x in this_hour])/len(this_hour))
-            min_ram.append(min([x.free_ram for x in this_hour]))
-            ave_ram.append(sum([x.free_ram for x in this_hour])/len(this_hour))
-            sd.append(min([x.cpu_temp for x in this_hour]))
-
-    line_cpu = Line(width=width, height=height, title='CPU 监控')
-    line_space = Line(width=width, height=height, title='DISK 监控')
-    if time.localtime().tm_hour >= 21 or time.localtime().tm_hour <= 6:
-        line_cpu.use_theme('dark')
-        line_space.use_theme('dark')
-    line_cpu.add('使用率峰值', stamps, max_cpu_percent, is_smooth=True, line_width=2)
-    line_cpu.add('平均使用率', stamps, ave_cpu_percent, is_smooth=True, line_width=2)
-    line_cpu.add('温度峰值', stamps, max_cpu_temp, is_smooth=True, mark_line=['max'], line_width=2)
-    line_cpu.add('平均温度', stamps, ave_cpu_temp, is_smooth=True, line_width=2, legend_top='bottom')
-    line_space.add('剩余内存谷值', stamps, min_ram, is_smooth=True, line_width=2)
-    line_space.add('剩余内存均值', stamps, ave_ram, is_smooth=True, line_width=2)
-    line_space.add('剩余磁盘', stamps, sd, is_smooth=True, line_width=2, legend_top='bottom')
-
-    js_list = list(set(line_cpu.get_js_dependencies() + line_space.get_js_dependencies()))
-
-    return line_cpu, line_space, js_list
+#     return line_cpu, line_space, js_list
 
 
-@app.route("/info/month")
-def last_month():
-    # 按天显示
-    end = int(time.time())
-    start = end - 86400 * 30
+# def group_line_maker(start, end, group_by):
+#     # 子函数：获取数据
+#     # 己函数：把数据放到 Line 中
+#     step = {
+#         'minute': 2,
+#         'hour': 3540,
+#         'day': 86340
+#     }[group_by]
 
-    line_cpu, line_space, js_list = group_line_maker(start, end, 'day')
+#     stamps = []
+#     max_cpu_percent = []
+#     ave_cpu_percent = []
+#     max_cpu_temp = []
+#     ave_cpu_temp = []
+#     min_ram = []
+#     ave_ram = []
+#     sd = []
+#     records = get_info(start, end)
 
-    return render_template(
-        "pyecharts.html",
-        cpu=line_cpu.render_embed(),
-        space=line_space.render_embed(),
-        host='https://pyecharts.github.io/assets/js',
-        script_list=js_list,
-    )
+#     for i in range(start + 60, end, step + 60):
+#         stamps.append(time.strftime('%d %H:%M', time.gmtime(i + 28800 - step)))
+#         this_hour = [v for r, v in records.items() if v.now > i and v.now < i + step]
+#         if not this_hour:
+#             max_cpu_percent.append(0)
+#             ave_cpu_percent.append(0)
+#             max_cpu_temp.append(0)
+#             ave_cpu_temp.append(0)
+#             min_ram.append(0)
+#             ave_ram.append(0)
+#             sd.append(0)
+#         else:
+#             max_cpu_percent.append(max([x.cpu_percent for x in this_hour]))
+#             ave_cpu_percent.append(sum([x.cpu_percent for x in this_hour])/len(this_hour))
+#             max_cpu_temp.append(max([x.cpu_temp for x in this_hour]))
+#             ave_cpu_temp.append(sum([x.cpu_temp for x in this_hour])/len(this_hour))
+#             min_ram.append(min([x.free_ram for x in this_hour]))
+#             ave_ram.append(sum([x.free_ram for x in this_hour])/len(this_hour))
+#             sd.append(min([x.cpu_temp for x in this_hour]))
 
+#     line_cpu = Line(width=width, height=height, title='CPU 监控')
+#     line_space = Line(width=width, height=height, title='DISK 监控')
+#     if time.localtime().tm_hour >= 21 or time.localtime().tm_hour <= 6:
+#         line_cpu.use_theme('dark')
+#         line_space.use_theme('dark')
+#     line_cpu.add('使用率峰值', stamps, max_cpu_percent, is_smooth=True, line_width=2)
+#     line_cpu.add('平均使用率', stamps, ave_cpu_percent, is_smooth=True, line_width=2)
+#     line_cpu.add('温度峰值', stamps, max_cpu_temp, is_smooth=True, mark_line=['max'], line_width=2)
+#     line_cpu.add('平均温度', stamps, ave_cpu_temp, is_smooth=True, line_width=2, legend_top='bottom')
+#     line_space.add('剩余内存谷值', stamps, min_ram, is_smooth=True, line_width=2)
+#     line_space.add('剩余内存均值', stamps, ave_ram, is_smooth=True, line_width=2)
+#     line_space.add('剩余磁盘', stamps, sd, is_smooth=True, line_width=2, legend_top='bottom')
 
-@app.route("/info/week")
-def last_week():
-    # 按天显示
-    end = int(time.time())
-    start = end - 86400 * 8
+#     js_list = list(set(line_cpu.get_js_dependencies() + line_space.get_js_dependencies()))
 
-    line_cpu, line_space, js_list = group_line_maker(start, end, 'day')
-
-    return render_template(
-        "pyecharts.html",
-        cpu=line_cpu.render_embed(),
-        space=line_space.render_embed(),
-        host='https://pyecharts.github.io/assets/js',
-        script_list=js_list,
-    )
-
-
-@app.route("/info/day")
-def last_day():
-    # 按小时显示
-    dawn = int(time.time() / 86400) * 86400 - 28800
-    end = int(time.time())
-
-    try:
-        days = request.args.get('days')
-        start = end - 86400 * int(days)
-    except:
-        start = end - 86400 * 1
-
-    line_cpu, line_space, js_list = group_line_maker(start, end, 'hour')
-
-    return render_template(
-        "pyecharts.html",
-        cpu=line_cpu.render_embed(),
-        space=line_space.render_embed(),
-        host='https://pyecharts.github.io/assets/js',
-        script_list=js_list,
-    )
-
-
-@app.route("/info")
-@app.route("/info/recent")
-def recent():
-    # dawn = int(time.time() / 86400) * 86400 - 28800
-    end = int(time.time())
-    # end = 1539100383
-    try:
-        hours = request.args.get('hours')
-        start = end - 3600 * int(hours)
-    except:
-        start = end - 3600 * 1
-
-    # 按照分钟显示
-
-    line_cpu, line_space, js_list = single_line_maker(start, end)
+#     return line_cpu, line_space, js_list
 
 
-    return render_template(
-        "pyecharts.html",
-        cpu=line_cpu.render_embed(),
-        space=line_space.render_embed(),
-        host='https://pyecharts.github.io/assets/js',
-        script_list=js_list,
-    )
+# @app.route("/info/month")
+# def last_month():
+#     # 按天显示
+#     end = int(time.time())
+#     start = end - 86400 * 30
+
+#     line_cpu, line_space, js_list = group_line_maker(start, end, 'day')
+
+#     return render_template(
+#         "pyecharts.html",
+#         cpu=line_cpu.render_embed(),
+#         space=line_space.render_embed(),
+#         host='https://pyecharts.github.io/assets/js',
+#         script_list=js_list,
+#     )
+
+
+# @app.route("/info/week")
+# def last_week():
+#     # 按天显示
+#     end = int(time.time())
+#     start = end - 86400 * 8
+
+#     line_cpu, line_space, js_list = group_line_maker(start, end, 'day')
+
+#     return render_template(
+#         "pyecharts.html",
+#         cpu=line_cpu.render_embed(),
+#         space=line_space.render_embed(),
+#         host='https://pyecharts.github.io/assets/js',
+#         script_list=js_list,
+#     )
+
+
+# @app.route("/info/day")
+# def last_day():
+#     # 按小时显示
+#     dawn = int(time.time() / 86400) * 86400 - 28800
+#     end = int(time.time())
+
+#     try:
+#         days = request.args.get('days')
+#         start = end - 86400 * int(days)
+#     except:
+#         start = end - 86400 * 1
+
+#     line_cpu, line_space, js_list = group_line_maker(start, end, 'hour')
+
+#     return render_template(
+#         "pyecharts.html",
+#         cpu=line_cpu.render_embed(),
+#         space=line_space.render_embed(),
+#         host='https://pyecharts.github.io/assets/js',
+#         script_list=js_list,
+#     )
+
+
+# @app.route("/info")
+# @app.route("/info/recent")
+# def recent():
+#     # dawn = int(time.time() / 86400) * 86400 - 28800
+#     end = int(time.time())
+#     # end = 1539100383
+#     try:
+#         hours = request.args.get('hours')
+#         start = end - 3600 * int(hours)
+#     except:
+#         start = end - 3600 * 1
+
+#     # 按照分钟显示
+
+#     line_cpu, line_space, js_list = single_line_maker(start, end)
+
+
+#     return render_template(
+#         "pyecharts.html",
+#         cpu=line_cpu.render_embed(),
+#         space=line_space.render_embed(),
+#         host='https://pyecharts.github.io/assets/js',
+#         script_list=js_list,
+#     )
 
 
 @app.route("/")
